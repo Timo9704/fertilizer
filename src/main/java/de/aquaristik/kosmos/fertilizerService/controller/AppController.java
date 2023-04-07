e2dc51f0678ae8c372a0352cedf1d39c0045cf45
Timo9704 <51120668+Timo9704@users.noreply.github.com>
2023-04-07 17:30:00 +0200

    Update AppController.java

--- src/main/java/de/aquaristik/kosmos/fertilizerService/controller/AppController.java
+++ src/main/java/de/aquaristik/kosmos/fertilizerService/controller/AppController.java
@@ -2,8 +2,10 @@ package de.aquaristik.kosmos.fertilizerService.controller;
 
 import com.google.gson.Gson;
 import de.aquaristik.kosmos.fertilizerService.model.Aquarium;
-import de.aquaristik.kosmos.fertilizerService.service.AquariumService;
+import de.aquaristik.kosmos.fertilizerService.model.DoseMixer;
 import de.aquaristik.kosmos.fertilizerService.model.Fertilizer;
+import de.aquaristik.kosmos.fertilizerService.service.AquariumService;
+import de.aquaristik.kosmos.fertilizerService.service.DoseMixerService;
 import de.aquaristik.kosmos.fertilizerService.service.FertilizerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -20,6 +25,10 @@ public class AppController {
     @Autowired
     FertilizerService fertilizerService;
 
+    @Autowired
+    DoseMixerService doseMixerService;
+
+
     @PostMapping(value = "/convert")
     public ResponseEntity convert(@RequestBody String jsonString){
         Gson gson = new Gson();
@@ -27,11 +36,13 @@ public class AppController {
 
         int[] fertilizerInUse = aquarium.getFertilizerInUse();
 
+        fertilizerService.init();
+
         List<Fertilizer> respList = new ArrayList<>();
         for (int i = 0; i < fertilizerInUse.length; i++) {
             Fertilizer fertilizer = fertilizerService.getFertilizer(fertilizerInUse[i]);
             fertilizer.calculateForAquarium(aquarium.getLiter());
-            fertilizer.setDosage(1);
+            fertilizer.setDosageModfiy(aquarium.getDosage());
             respList.add(fertilizer);
         }
         return ResponseEntity.ok(respList);
@@ -63,8 +74,32 @@ public class AppController {
 
         double[] consumptionArray = aquariumService.consumption();
 
+        int[] fertilizerInUse = aquarium.getFertilizerInUse();
 
+        fertilizerService.init();
 
-        return ResponseEntity.ok("Ok");
+        List<Fertilizer> fertilizerList = new ArrayList<>();
+        for (int i = 0; i < fertilizerInUse.length; i++) {
+            Fertilizer fertilizer = fertilizerService.getFertilizer(fertilizerInUse[i]);
+            fertilizer.calculateForAquariumPrecise(aquarium.getLiter());
+            fertilizerList.add(fertilizer);
+        }
+
+        DoseMixer doseMixer = new DoseMixer(consumptionArray[0], consumptionArray[1], consumptionArray[2], consumptionArray[3], fertilizerList);
+
+        boolean optimalDosage = doseMixerService.calculateDosage(fertilizerList, doseMixer);
+
+        Map<String, Double> resp = new HashMap<>();
+
+        if(optimalDosage) {
+            System.out.println(fertilizerList.get(0).getName() + " " + fertilizerList.get(0).getDosage());
+            for (int i = 0; i < fertilizerList.size(); i++) {
+                resp.put(String.valueOf(fertilizerList.get(i).getName()), fertilizerList.get(i).getDosage());
+            }
+        }else{
+            resp.put("Error", 0.0);
+        }
+
+        return ResponseEntity.ok(resp);
     }
 }
